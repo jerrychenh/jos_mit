06bed27fce7205b08cd395b6983fe822e63f813c
Jerry Chen <jerrychen@nd.com.cn>
2019-09-15 13:48:47 +0800

    lab3 exc 8

--- user/hello.c
+++ user/hello.c
@@ -5,5 +5,7 @@ void
 umain(int argc, char **argv)
 {
 	cprintf("hello, world\n");
+	// at first thisenv is not set in libmain.c, so this pointer value is 0, 
+	// deref it will cause page fault
 	cprintf("i am environment %08x\n", thisenv->env_id);
 }
